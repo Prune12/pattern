7029734d128323cfae9b49533e12b9c8a45a280e
Prune12 <prunellengolong13@gmail.com>
2023-10-07 13:58:45 +0100

    premier patron abstract factory contenant deux familles

---
+++ src/Client.java
@@ -0,0 +1,21 @@
+public class Client {
+    public static void main(String[] args) {
+        IproduitFactory produitfactory1=new ProduitFactory1();
+        IproduitFactory produitfactory2=new ProduitFactory2();
+
+        ProduitA produitA =null;
+        ProduitB produitB=null;
+
+        System.out.println("utilisation de la premiere fabrique");
+        produitA = produitfactory1.getProduitA();
+        produitB= produitfactory1.getProduitB();
+        produitA.methodA();
+        produitB.methodB();
+
+        System.out.println("utilisation de la deuxieme fabrique");
+        produitA = produitfactory2.getProduitA();
+        produitB= produitfactory2.getProduitB();
+        produitA.methodA();
+        produitB.methodB();
+    }
+}
